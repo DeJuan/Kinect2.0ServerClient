0ed5efb94268857a40d18f5ff5ffa0941b44045a
DeJuan <dejuan@mit.edu>
2015-01-14 17:48:35 -0500

    First draft of server is done. moving to client.

--- UDPServer.cpp
+++ UDPServer.cpp
@@ -17,7 +17,9 @@ namespace udp_server
 	 * It can either directly be the host name or just the numbers- let's use numbers. 
 	 *On failure to resolve the address, the server will error and the constructor will throw an exception.
 	 * Don't fork() or exec() in this, it'll close the socket!
-	 *
+	 * Constructor time!
+	 * \param addr : The address to convert to a numeric IP.
+	 * \param port : The port number to use for the connection.
 	 */
 	udp_server::udp_server(const std::string& addr, int port) : f_port(port), f_addr(addr)
 	{
@@ -30,6 +32,85 @@ namespace udp_server
 		hints.ai_family = AF_UNSPEC;
 		hints.ai_socktype = SOCK_DGRAM;
 		hints.ai_protocol = IPPROTO_UDP;
-		int r(getaddrinfo(addr.c_str(), decimal_port, &hints, &f_addrinfo));
+		int r(getaddrinfo(addr.c_str(), decimal_port, &hints, &f_addrinfo)); //automatically get the info for the address given, error # stored in r
+		if (r != 0 || f_addrinfo == NULL){ //if we couldn't get the info or we got an error return (-1), didn't work.
+			throw udp_server_runtime_error(("Invalid address or port given: " + addr + ":" + decimal_port).c_str());
+		}
+
+		f_socket = socket(f_addrinfo->ai_family, SOCK_DGRAM, IPPROTO_UDP); //make socket according to protocol
+		if (f_socket == -1){
+			freeaddrinfo(f_addrinfo);
+			throw udp_server_runtime_error(("Could not create a socket for " + addr + ":" + decimal_port).c_str());
+		}
+
+		r = bind(f_socket, f_addrinfo->ai_addr, f_addrinfo->ai_addrlen); //bind the socket we just made
+		if (r != 0)
+		{
+			freeaddrinfo(f_addrinfo);
+			close(f_socket);
+			throw udp_server_runtime_error(("Could not bind the socket to " + addr + ":" + decimal_port).c_str());
+		}
 	}
+
+	/**
+	 *\brief This is the destructor for the UPD server. It'll free the address info struct and close the socket before
+	 * it returns.
+	 */
+	udp_server::~udp_server()
+	{
+		freeaddrinfo(f_addrinfo);
+		close(f_socket);
+	}
+
+	/**
+	 * \brief This will return a copy of the socket identifier, which you can use to change flags if you want. 
+	 */
+	int udp_server::get_socket() const
+	{
+		return f_socket;
+	}
+
+	/**
+	* \brief This will return the port being used by the server.
+	*/
+	int udp_server::get_port() const
+	{
+		return f_port;
+	}
+
+	/** \brief Retrieve a copy of the address being used as a string.
+	*
+	* This function returns a copy of the address as it was specified in the
+	* constructor. This does not return a canonalized version of the address.
+	*
+	* The address cannot be modified. If you need to send data on a different
+	* address, create a new UDP server.
+	*
+	* \return A string with a copy of the constructor input address.
+	*/
+	std::string udp_server::get_addr() const
+	{
+		return f_addr;
+	}
+
+	/** \brief Send a message from this UDP server.
+	*
+	* This function sends a message through the UDP socket. The function
+	* cannot be used to change the destination as it was defined when creating
+	* the udp_server object.
+	*
+	* The size must be large enough for the message to fit. 
+	*
+	* \param[in] msg  The message to send.
+	* \param[in] size  The number of bytes representing this message.
+	*
+	* \return -1 if an error occurs, otherwise the number of bytes sent. The error number
+	* is set accordingly on error.
+	*/
+	int udp_server::send(const char *msg, size_t size)
+	{
+		return sendto(f_socket, msg, size, 0, f_addrinfo->ai_addr, f_addrinfo->ai_addrlen);
+	}
+
+
 }
